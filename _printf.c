477a2b18d39a1959f9d1abe93c32e9e6592a4a64
root <root@LAPTOP-FAU10JKU>
2025-03-27 15:55:26 +0100

    implémentation de printf

--- _printf.c
+++ _printf.c
@@ -9,30 +9,59 @@
  */
 int _printf(const char *format, ...)
 {
-	va_list args;
-	int count = 0;
+    va_list args;
+    int count = 0;
 
-	if (!format)
-		return (-1);
+    if (!format)
+        return (-1);
 
-	va_start(args, format);
+    va_start(args, format);
 
-	while (*format)
-	{
-		if (*format == '%' && (*(format + 1) == 'd' || *(format + 1) == 'i'))
-		{
-			count += print_int(va_arg(args, int));
-			format += 2;
-		}
-		else
-		{
-			write(1, format, 1);
-			count++;
-			format++;
-		}
-	}
+    while (*format)
+    {
+        if (*format == '%')  /* Détecte un formatage */
+        {
+            format++; /* Passe au caractère suivant */
 
-	va_end(args);
-	return (count);
+            if (*format == 'c')  /* Gestion du %c */
+            {
+                char c = va_arg(args, int);
+                write(1, &c, 1);
+                count++;
+            }
+            else if (*format == 's') /* Gestion du %s */
+            {
+                char *str = va_arg(args, char *);
+                if (!str)
+                    str = "(null)"; /* Pour éviter les erreurs avec NULL */
+                while (*str)
+                {
+                    write(1, str, 1);
+                    str++;
+                    count++;
+                }
+            }
+            else if (*format == '%') /* Gestion de %% */
+            {
+                write(1, "%", 1);
+                count++;
+            }
+            else /* Si format inconnu, affiche % */
+            {
+                write(1, "%", 1);
+                write(1, format, 1);
+                count += 2;
+            }
+        }
+        else /* Caractère normal */
+        {
+            write(1, format, 1);
+            count++;
+        }
+        format++;
+    }
+
+    va_end(args);
+    return (count);
 }
 
