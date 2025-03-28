cc8e2ae2623895a768953665bd20663e921d79fe
Nada <Nada@MacBook-Pro-de-Macbook.local>
2025-03-28 14:08:01 +0100

    modification of the prototypes of the subfonction

--- main.h
+++ main.h
@@ -1,3 +1,14 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdarg.h>
+#include <stddef.h>
+#include <unistd.h>
+
+/* Prototypes des fonctions */
 int _printf(const char *format, ...);
-int main(void);
 int _putchar(char c);
+int print_string(char *str);
+
+#endif /* MAIN_H */
+
